68739904b421619ac6cf16431669932215719390
Spencer Tipping <spencer@spencertipping.com>
2011-07-09 08:17:34 -0600

    Starting over with a traditional source-first implementation (i.e. no self-modifying perl).

--- core/init.rb
+++
@@ -1,4 +0,0 @@
-const_set :Rift, Module.new
-
-require 'core/lib/init'
-require 'core/tree/init'
--- core/lib/extensible.rb
+++
@@ -1,7 +0,0 @@
-Rift.const_set :Extensible, Module.new do
-  define_method :extensible_subclass do |name, &initializer|
-    child = Class.new self, &initializer
-    const_set name, child
-    self
-  end
-end
