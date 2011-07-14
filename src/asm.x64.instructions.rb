6b2ef50433fc7def4ac5e35631eb34292377795d
Spencer Tipping <spencer@spencertipping.com>
2011-07-14 13:26:41 -0600

    Writing the low-level assembler. Trying to figure out an appropriate model for register/memory stuff, which is tricky because of the restrictions imposed by various instructions.

--- src/asm.x64.instructions.rb
+++ src/asm.x64.instructions.rb
@@ -1,2 +1,17 @@
 # x64 instruction encoding | Spencer Tipping
 # Licensed under the terms of the MIT source code license
+
+# Low-level instruction streams for x86-64 processors. These are rendered
+# as byte arrays that can later be written to files or memory.
+class Rift::X64::LLASM
+
+  # Create a new low-level assembler with an empty instruction stream and no
+  # registered labels.
+  def initialize
+    @instructions, @labels, @unbound = [], {}, {}
+  end
+
+  # Move instructions. These are variants of 'movq' and moves to and from XMM
+  # registers.
+
+end
--- src/asm.x64.registers.rb
+++ src/asm.x64.registers.rb
@@ -1,9 +1,7 @@
 # x64 register definitions | Spencer Tipping
 # Licensed under the terms of the MIT source code license
 
-module Rift::X64
-  # Represents a physical register. This is used later on for register
-  # allocation.
-  class Register < Struct.new(:index)
-  end
+# Represents a physical register. This is used later on for register
+# allocation, as well as in the low-level assembler.
+class Rift::X64::Register < Struct.new(:index)
 end
