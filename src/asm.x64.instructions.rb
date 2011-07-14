1680c38f6dd6bb0f123778fdcc4c3329dd4ae6a6
Spencer Tipping <spencer@spencertipping.com>
2011-07-14 14:59:33 -0600

    Designing the instruction functions. Tempted to abandon the low-level representation in favor of two higher-level models that independently generate instructions using straight bytes.

--- src/asm.x64.instructions.rb
+++ src/asm.x64.instructions.rb
@@ -3,15 +3,29 @@
 
 # Low-level instruction streams for x86-64 processors. These are rendered
 # as byte arrays that can later be written to files or memory.
-class Rift::X64::LLASM
+class Rift::X64::InstructionAssembler
 
   # Create a new low-level assembler with an empty instruction stream and no
   # registered labels.
   def initialize
-    @instructions, @labels, @unbound = [], {}, {}
+    @stream, @labels, @unbound = [], {}, {}, 0
   end
 
-  # Move instructions. These are variants of 'movq' and moves to and from XMM
-  # registers.
+  def << byte
+    @stream << (byte & 0xff)
+    self
+  end
+  private :<<
 
+  def size
+    @stream.size
+  end
+
+  # Move instruction group.
+  # These are separated by case: reg/reg, reg/mem, mem/reg, mem/xmm, etc. The
+  # right-hand argument is the destination; that is, 'move X to Y', or AT&T
+  # assembler syntax.
+
+  def mov_rr i1, i2
+  end
 end
